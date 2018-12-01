d9e1296a2bae233ffbe1d4b9d87ddcafd8037e60
steverodri91 <steve20.pro@gmail.com>
2018-12-01 15:16:31 -0500

    created seed data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Express and React with One Model 
+# Express and React with One Model
 
 #### For this assignment, you will be building out the back-end followed by the front-end, using Express and React, respectively.
 
@@ -17,12 +17,12 @@ Please read through all sections before begininng.
 - Run `npm run seed` (after creating the seed data)
 - Run `npm run dev`
 
-We have provided you starter code, inside of `models.js`, and a skeleton of `server.js`. You will be providing the data for `seed.js`, so be creative! 
+We have provided you starter code, inside of `models.js`, and a skeleton of `server.js`. You will be providing the data for `seed.js`, so be creative!
 
 Look through the `seed.js` file, we have given you an example of how to write the data for Student. Write the data to insert into our three models.
 
 ### Deliverables
-1. Create four students. **You are not required to create seed data for Class or Intructor, we will build this out next week**
+1. Create four students. **You are not required to create seed data for Class or Instructor, we will build this out next week**
 1. In `server.js` initiate routes for the index route and the create route, delete is fair game(it will not be counted against your completion mark if it doesn't work).
 1.  Student routes should use `GET /students` and `POST /students` endpoints.
 1.  Please provide at least four logical commits when setting up the back-end and add comments where appropriate
@@ -40,7 +40,7 @@ Look through the `seed.js` file, we have given you an example of how to write th
 -  GET `/students`
 -  POST  `/students`
 3. Implement onChange and onSubmit handlers in the form
-4. Style your app using CSS, this would be a great time to practice grid or flexbox! Feeling adventurous? Look into [React-Bootstrap](https://react-bootstrap.github.io/) 
+4. Style your app using CSS, this would be a great time to practice grid or flexbox! Feeling adventurous? Look into [React-Bootstrap](https://react-bootstrap.github.io/)
 5. Please provide at least four logical commits when setting up the front-end and add comments where appropriate.
 
 
--- seed.js
+++ seed.js
@@ -2,11 +2,27 @@ const { Class, Student, Instructor } = require('./models');
 
 async function seed() {
   const students = await Student.bulkCreate([
-    // {
-    //   name: 'David',
-    //   hometown: 'Arlington',
-    //   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam arcu nec diam finibus, vel congue leo vulputate. Aliquam id laoreet nunc. Sed nec porttitor diam. Aenean et rutrum lorem. Aliquam ac commodo urna. Donec et turpis molestie, rutrum orci vitae, eleifend nisl. Etiam facilisis pharetra facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis risus enim, sit amet eleifend tellus cursus quis.'
-    // }
+    {
+      name: 'David',
+      hometown: 'Arlington',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam arcu nec diam finibus, vel congue leo vulputate. Aliquam id laoreet nunc. Sed nec porttitor diam. Aenean et rutrum lorem. Aliquam ac commodo urna. Donec et turpis molestie, rutrum orci vitae, eleifend nisl. Etiam facilisis pharetra facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis risus enim, sit amet eleifend tellus cursus quis.'
+    },
+    {
+      name: 'Sandy',
+      hometown: 'Denver',
+      bio:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      name: 'Trevor',
+      hometown: 'Los Angeles',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+      name:'Lisa',
+      hometown: 'Seattle',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+
   ]);
   process.exit()
 }
