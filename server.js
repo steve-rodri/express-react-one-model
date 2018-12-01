ed4cedb09801df9c6dcf6c3f586d7694696b7bc3
steverodri91 <steve20.pro@gmail.com>
2018-12-01 15:55:50 -0500

    created GET ALL students endpoint

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+//GET students
+app.get('/students', async (req, res) => {
+  try {
+    const students = await Student.findAll();
+    res.json({
+      students
+    })
+  } catch (e) {
+    console.log(e.message);
+    res.sendStatus(404);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
